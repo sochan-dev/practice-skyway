fdc56559fb792936c4586a853b667204152ce932
sotatsu kitajima <halosakakitajima@gmail.com>
2021-08-06 21:04:11 +0900

    条件のテスト

--- src/Room.tsx
+++ src/Room.tsx
@@ -141,6 +141,11 @@ const Room: VFC = () => {
     room.replaceStream(localVideo)
   }
 
+  // eslint-disable-next-line no-constant-condition
+  if (((1 <= 1 && 1 <= 1) || (1 <= 1 && 1 <= 1)) && ((1 <= 1 && 1 <= 1) || (1 <= 1 && 1 <= 1))) {
+    console.log('f')
+  }
+
   return (
     <>
       <div>
